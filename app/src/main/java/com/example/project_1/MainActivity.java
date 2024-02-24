8932d0e1874d8dbcfaea0810c9c4884ec0293f72
lourq <lourzzz666@gmail.com>
2024-02-24 21:24:12 +0200

    xo game

--- app/src/main/java/com/example/project_1/MainActivity.java
+++ app/src/main/java/com/example/project_1/MainActivity.java
@@ -1,85 +1,87 @@
 package com.example.project_1;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button b;
-    private TextView t;
-    private TextView headerText;
-    private Date targetDate;
-    private Date currentDate;
-    private String timeLeft;
-    private int buttonSize = 100;
-    private int buttonClickCount = 0;
+    private int xImage = R.drawable.x_image;
+    private int oImage = R.drawable.o_image;
+    private ImageView[] imageViews = new ImageView[9];
+    private int[] field = new int[9];
+    private boolean move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        b = findViewById(R.id.button);
-//        t = findViewById(R.id.textView);
-//        headerText = findViewById(R.id.headerTime);
-//        try {
-//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-//            targetDate = sdf.parse("2024-02-08 08:50:00");
-//
-//            currentDate = Calendar.getInstance().getTime();
-//
-//            long diff = targetDate.getTime() - currentDate.getTime();
-//
-//            long days = TimeUnit.MILLISECONDS.toDays(diff);
-//            long hours = TimeUnit.MILLISECONDS.toHours(diff) % 24;
-//            long minutes = TimeUnit.MILLISECONDS.toMinutes(diff) % 60;
-//            long seconds = TimeUnit.MILLISECONDS.toSeconds(diff) % 60;
-//
-//            timeLeft = String.format(Locale.getDefault(),
-//                    "Time Left -> %d д %d ч %d м %d секунд",
-//                    days, hours, minutes, seconds);
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        b.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                if(buttonClickCount < 20) {
-//                    buttonSize += 30;
-//                    buttonClickCount++;
-//                    b.getLayoutParams().width = buttonSize;
-//                    b.requestLayout();
-//                    t.setText(String.valueOf(buttonClickCount));
-//
-//                } else {
-//                    b.setBackgroundColor(Color.RED);
-//                    b.setText("Broken T^T");
-//                }
-//
-////                Toast.makeText(MainActivity.this,
-////                        String.valueOf(timeLeft),
-////                        Toast.LENGTH_SHORT).show();
-//                headerText.setText(timeLeft);
-//            }
-//        });
+        // INFO: assigning id to handlers , add num to field
+        for(int i = 0 ; i < imageViews.length ; i++){
+            String imageViewId = "imageView" + (i + 1);
+            int resId = getResources().getIdentifier(imageViewId, "id", getPackageName());
+            imageViews[i] = findViewById(resId);
+            field[i] = i;
+        }
+        move = false;
     }
 
-    public void func(View view) {
-        Toast.makeText(this, "KNP202", Toast.LENGTH_SHORT).show();
+    public void onCellClicked(View v) {
+        for(int i = 0 ; i < imageViews.length ; i++){
+            if(v.getId() == imageViews[i].getId()){
+                if (!move) {
+                    imageViews[i].setImageResource(xImage);
+                    field[i] = 1;
+                } else {
+                    imageViews[i].setImageResource(oImage);
+                    field[i] = 2;
+                }
+                move = !move;
+            }
+        }
+        if(checkWinner()) {
+            String winner = "Player " + (move ? "X" : "O") + " Winner";
+            Toast.makeText(MainActivity.this,
+                    String.valueOf(winner),
+                    Toast.LENGTH_SHORT).show();
+            resetGame();
+        }
+    }
+
+    private boolean checkWinner() {
+        // INFO: checking horizontal lines
+        for (int i = 0; i < 9; i += 3) {
+            if (field[i] == field[i + 1] && field[i] == field[i + 2] && field[i] != 0) {
+                return true;
+            }
+        }
+
+        // INFO: checking vertical lines
+        for (int i = 0; i < 3; i++) {
+            if (field[i] == field[i + 3] && field[i] == field[i + 6] && field[i] != 0) {
+                return true;
+            }
+        }
+
+        // INFO: checking diagonal lines
+        if (field[0] == field[4] && field[0] == field[8] && field[0] != 0) {
+            return true;
+        }
+        if (field[2] == field[4] && field[2] == field[6] && field[2] != 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < imageViews.length; i++) {
+            imageViews[i].setImageResource(0);
+            field[i] = 0;
+        }
+        move = false;
     }
 }
